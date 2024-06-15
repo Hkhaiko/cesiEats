7d173ae9d88391fdbb781703a021f71a0930f3d5
Simon Bello <simon.bello@viacesi.fr>
2024-06-15 16:18:16 +0200

    Homepage and profil page

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AccountPage from './pages/AccountPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<AccountPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,52 @@
+// src/pages/HomePage.js
+import React from 'react';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <Container className="home-page">
+      <Row className="justify-content-center mt-4">
+        <Col xs={12} className="text-center">
+          <img src="logo.svg" alt="Logo" className="mb-4 logo" />
+        </Col>
+        <Col xs={6} className="text-center">
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Text>Revenu</Card.Text>
+              <Card.Text className="text-success">+153,40 €</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={6} className="text-center">
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Text>Score</Card.Text>
+              <Card.Text>1260</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12}>
+          <Button variant="light" className="w-100 mb-3" onClick={() => navigate('/profile')}>Modifier Profil</Button>
+          <Button variant="light" className="w-100 mb-3">Historique des livraisons</Button>
+          <Button variant="light" className="w-100 mb-3">Code Parrainage</Button>
+        </Col>
+        <Col xs={12} className="text-center">
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Text>Vous êtes actuellement</Card.Text>
+              <Card.Text>Actif</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12}>
+          <Button variant="danger" className="w-100">PASSER HORS-LIGNE</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HomePage;
