8e2dd3858911e5d5392042bb4e656c0ab2c53332
Simon Bello <simon.bello@viacesi.fr>
2024-06-18 01:16:22 +0200

    page orders and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,16 @@
 // src/App.js
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AccountPage from './pages/AccountPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import PrivateRoute from './components/PrivateRoute';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-function AppContent() {
-  const location = useLocation();
-  const isAuthRoute = location.pathname === '/login' || location.pathname === '/register';
-
-  return (
-    <>
-      {!isAuthRoute && <Header />}
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="*"
-          element={
-            <PrivateRoute>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/profile" element={<AccountPage />} />
-              </Routes>
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-      {!isAuthRoute && <Footer />}
-    </>
-  );
-}
+import OrderPage from './pages/OrderPage';
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <AppContent />
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/order" element={<OrderPage />} />
+      </Routes>
     </Router>
   );
 }
