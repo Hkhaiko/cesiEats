716f04197e336d954dc2c68825c9cc24ca72f2a4
Simon Bello <simon.bello@viacesi.fr>
2024-06-19 10:42:41 +0200

    solve password problem

--- backend/models/deliveryPersonModel.js
+++ backend/models/deliveryPersonModel.js
@@ -57,6 +57,15 @@ deliveryPersonSchema.pre("save", async function (next) {
   next();
 });
 
+deliveryPersonSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate();
+  if (update.password) {
+    const salt = await bcrypt.genSalt(10);
+    update.password = await bcrypt.hash(update.password, salt);
+  }
+  next();
+});
+
 deliveryPersonSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
